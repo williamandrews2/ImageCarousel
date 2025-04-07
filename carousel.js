1078cdd07b3fb7825fdc38d8dfcd0e6fe3d4a5fd
William A. <121400607+williamandrews2@users.noreply.github.com>
2025-04-07 10:34:00 -0500

    Created basic functionality of slideshow.

--- carousel.js
+++ carousel.js
@@ -1,3 +1,27 @@
 let i = 0;
 let images = [];
-let time = 2000; // time in ms
+let time = 3000; // time in ms
+let slide = document.getElementById("slide");
+
+// image list
+images[0] = "./images/darkpolygons.jpg";
+images[1] = "./images/orangecat.jpg";
+images[2] = "./images/blackcat.jpg";
+images[3] = "./images/tuxedocat.jpg";
+
+// change image
+function changeImg() {
+  slide.src = images[i];
+
+  if (i < images.length - 1) {
+    i++;
+  } else {
+    i = 0;
+  }
+
+  setTimeout(() => {
+    changeImg();
+  }, time);
+}
+
+window.onload = changeImg;
