80b7a88d15b810a042a1ca210d0b044766bdb6bb
William A. <121400607+williamandrews2@users.noreply.github.com>
2025-04-07 11:23:10 -0500

    Refined carousel.js and added functionality for manual navigation with wrap around.

--- carousel.js
+++ carousel.js
@@ -1,7 +1,11 @@
 let i = 0;
 let images = [];
 let time = 3000; // time in ms
-let slide = document.getElementById("slide");
+let slideInterval;
+
+const slide = document.getElementById("slide");
+const previousButton = document.getElementById("previous-button");
+const nextButton = document.getElementById("next-button");
 
 // image list
 images[0] = "./images/darkpolygons.jpg";
@@ -9,19 +13,42 @@ images[1] = "./images/orangecat.jpg";
 images[2] = "./images/blackcat.jpg";
 images[3] = "./images/tuxedocat.jpg";
 
+// event listeners for manual navigation
+previousButton.addEventListener("click", () => {
+  changeIndex(-1);
+  changeImg();
+  resetInterval();
+});
+
+nextButton.addEventListener("click", () => {
+  changeIndex(1);
+  changeImg();
+  resetInterval();
+});
+
 // change image
 function changeImg() {
   slide.src = images[i];
+}
 
-  if (i < images.length - 1) {
-    i++;
-  } else {
-    i = 0;
-  }
+// change index: -1 for previous and 1 for next
+function changeIndex(direction) {
+  i = (i + direction + images.length) % images.length;
+}
 
-  setTimeout(() => {
+function resetInterval() {
+  clearInterval(slideInterval);
+  startSlideshow();
+}
+
+function startSlideshow() {
+  slideInterval = setInterval(() => {
     changeImg();
+    changeIndex(1);
   }, time);
 }
 
-window.onload = changeImg;
+window.onload = () => {
+  changeImg();
+  startSlideshow();
+};
